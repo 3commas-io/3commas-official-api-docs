149c9cdb4d8dac9b3665c11c4d9712f93c0e4931
Erik Farukshin <nctilucent@gmail.com>
2025-01-21 15:27:56 +0100

    chore: refactor

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,12 +1,16 @@
 import React, { ComponentProps } from 'react'
-import * as TooltipPrimitive from '@radix-ui/react-tooltip'
+import * as Primitive from '@radix-ui/react-tooltip'
 import styles from './Tooltip.module.css'
+
+const preventDefault: React.EventHandler<any> = (event) => {
+  event.preventDefault()
+}
 
 type Props = {
   children: React.ReactNode
   content: React.ReactNode
 } & Pick<
-  ComponentProps<typeof TooltipPrimitive.Root>,
+  ComponentProps<typeof Primitive.Root>,
   'open' | 'defaultOpen' | 'onOpenChange'
 >
 
@@ -19,31 +23,25 @@ export function Tooltip({
   ...props
 }: Props) {
   return (
-    <TooltipPrimitive.TooltipProvider>
-      <TooltipPrimitive.Root
+    <Primitive.Provider>
+      <Primitive.Root
         open={open}
         defaultOpen={defaultOpen}
         onOpenChange={onOpenChange}
         delayDuration={100}>
-        <TooltipPrimitive.Trigger
-          asChild
-          onClick={(event) => {
-            event.preventDefault()
-          }}>
+        <Primitive.Trigger asChild onClick={preventDefault}>
           {children}
-        </TooltipPrimitive.Trigger>
-        <TooltipPrimitive.Content
+        </Primitive.Trigger>
+        <Primitive.Content
           {...props}
           side="top"
           align="center"
           className={styles.content}
-          onPointerDownOutside={(event) => {
-            event.preventDefault()
-          }}>
-          <TooltipPrimitive.Arrow className={styles.arrow} offset={10} />
+          onPointerDownOutside={preventDefault}>
+          <Primitive.Arrow className={styles.arrow} offset={10} />
           {content}
-        </TooltipPrimitive.Content>
-      </TooltipPrimitive.Root>
-    </TooltipPrimitive.TooltipProvider>
+        </Primitive.Content>
+      </Primitive.Root>
+    </Primitive.Provider>
   )
 }
