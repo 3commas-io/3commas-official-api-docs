0d6de5393442453801bd2ac63bc76bb7f9cdf0cb
Erik Farukshin <nctilucent@gmail.com>
2025-01-23 14:01:06 +0100

    feat: adds sticky layout

---
+++ src/components/StickyLayout/index.tsx
@@ -0,0 +1,17 @@
+import styles from './StickyLayout.module.css'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export function Root({ children }: Props) {
+  return <div className={styles.root}>{children}</div>
+}
+
+export function Sticky({ children }: Props) {
+  return <div className={styles.sticky}>{children}</div>
+}
+
+export function Main({ children }: Props) {
+  return <div>{children}</div>
+}
