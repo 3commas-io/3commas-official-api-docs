522a24688aff3a4d41e0edc4eada12cbba187246
Nazar Kuchynsky <n.kuchynskyi@3commas.io>
2025-02-26 20:47:58 +0100

    Fixed scroll trouble + fixed sidebar buttons

--- src/components/ScrollStylingManager/ScrollStylingManager.tsx
+++ src/components/ScrollStylingManager/ScrollStylingManager.tsx
@@ -1,24 +1,73 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 
 const ScrollStylingManager = () => {
     useEffect(() => {
+        const excludedRoutes = [
+            '/account/',
+            '/account/account-entity',
+            '/dca-bot',
+            '/dca-bot/deals/',
+            '/dca-bot/dca-bot-entity',
+            '/dca-bot/deals/deal-entity',
+            '/general-information',
+            '/general-information/dates-and-times',
+            '/general-information/errors',
+            '/general-information/get-an-api-key',
+            '/general-information/how-to-create-an-rsa-key-pair',
+            '/general-information/how-to-start',
+            '/general-information/limits',
+            '/general-information/overview',
+            '/general-information/security-and-permission-types',
+            '/general-information/signing-a-request-using-hmac-sha256',
+            '/general-information/signing-a-request-using-rsa',
+            '/grid-bot',
+            '/grid-bot/grid-bot-entity',
+            '/market-data',
+            '/smart-trade/',
+            '/smart-trade/smart-trade-entity',
+            '/smart-trade/trades/trade-entity'
+        ];
+
         const handleScroll = () => {
-            const selectors = [
-                '.breadcrumbsContainer_Ar0W',
-                '.theme-doc-markdown header h1',
-                '.container_eK_a'
-            ];
-            const targetElements = document.querySelectorAll(selectors.join(', '));
+            const isExcluded = excludedRoutes.includes(window.location.pathname);
+
+            const breadcrumbs = document.querySelector('.breadcrumbsContainer_Ar0W');
+            const container = document.querySelector('.container_eK_a');
+            const header = document.querySelector('.theme-doc-markdown header h1');
 
             if (window.scrollY >= 140) {
-                targetElements.forEach(el => el.classList.add('scrolledClass'));
+                // breadcrumbsContainer_Ar0W завжди отримує scrolledClass
+                if (breadcrumbs) breadcrumbs.classList.add('scrolledClass');
+
+                // theme-doc-markdown header h1 отримує клас залежно від маршруту
+                if (header) {
+                    if (isExcluded) {
+                        header.classList.add('scrolledClassMain');
+                        header.classList.remove('scrolledClass');
+                    } else {
+                        header.classList.add('scrolledClass');
+                        header.classList.remove('scrolledClassMain');
+                    }
+                }
+
+                // container_eK_a отримує клас тільки на дозволених маршрутах
+                if (!isExcluded && container) {
+                    container.classList.add('scrolledClass');
+                }
             } else {
-                targetElements.forEach(el => el.classList.remove('scrolledClass'));
+                // Видаляємо класи при скролі вгору
+                if (breadcrumbs) breadcrumbs.classList.remove('scrolledClass');
+                if (header) {
+                    header.classList.remove('scrolledClass');
+                    header.classList.remove('scrolledClassMain');
+                }
+                if (container) container.classList.remove('scrolledClass');
             }
         };
 
         window.addEventListener('scroll', handleScroll);
         handleScroll();
+        console.log(window.location.pathname)
 
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
