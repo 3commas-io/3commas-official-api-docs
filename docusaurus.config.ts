059d6370aaa7d5a7224d36399212218fd4d5b87d
VitaliiSalinskyi <v.salinskiy@3commas.io>
2025-02-27 18:14:10 +0100

    chore: add custom light theme

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,7 @@
 import { themes as prismThemes } from 'prism-react-renderer'
 import type { Config } from '@docusaurus/types'
 import type * as Preset from '@docusaurus/preset-classic'
+import customLightTheme from "./src/utils/lightTheme"
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -66,7 +67,7 @@ const config: Config = {
       style: 'dark',
     },
     prism: {
-      theme: prismThemes.github,
+      theme: customLightTheme,
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
