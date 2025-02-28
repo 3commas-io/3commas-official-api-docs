95b08f160c123d05805a35a27b47030880aea51b
VitaliiSalinskyi <v.salinskiy@3commas.io>
2025-02-28 12:54:45 +0100

    refactor: replace example text with Separator
    - add label customization

--- src/components/Separator/index.tsx
+++ src/components/Separator/index.tsx
@@ -1,8 +1,28 @@
+import { CSSProperties } from "react";
+import clsx from "clsx";
+
 import styles from "./Separator.module.css"
 
-const Separator = () => {
+interface SeparatorProps {
+  label?: string;
+  labelBg?: { dark?: string, light?: string };
+}
+
+export const ADDITIONAL_INFORMATION_THEME: SeparatorProps["labelBg"] = {
+  light: "#fdfdfe",
+  dark: "#474748"
+}; 
+
+const Separator = ({label = "", labelBg }: SeparatorProps) => {
   return (
-    <hr className={styles.separator}/>
+    <hr 
+      data-label={label}
+      className={clsx(styles.separator, label && styles.example)} 
+      style={{
+        "--separator-text-bg-dark": labelBg?.dark || "var(--ifm-background-color)",
+        "--separator-text-bg-light": labelBg?.light || "#fff",
+      } as CSSProperties}
+    />
   )
 }
 
