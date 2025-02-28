887a3dee8f82bb6ec67c1921ce8118ea7609dd38
VitaliiSalinskyi <v.salinskiy@3commas.io>
2025-02-28 11:36:38 +0100

    refactor: replace parameter ranges with ParameterRange component
    - add optional labels

--- src/components/ApiParam/ParameterRange.tsx
+++ src/components/ApiParam/ParameterRange.tsx
@@ -6,24 +6,29 @@ interface ParameterRangeProps {
   min?: number | string;
   max?: number | string;
   defaultVal?: number | string;
+  labels?: {
+    min?: string;
+    max?: string;
+    default?: string;
+  }
 }
 
-export const ParameterRange = ({ defaultVal, max, min }: ParameterRangeProps) => {
+export const ParameterRange = ({ defaultVal, max, min, labels = { default: "default", min: "min", max: "max" } }: ParameterRangeProps) => {
   return (
     <div className={styles['parameter-range']}>
       {min !== undefined && (
         <div className={clsx(styles.min, styles.value)}>
-          min: <code>{min}</code>
+          {labels.min}: <code>{min}</code>
         </div>
       )}
       {max !== undefined && (
         <div className={clsx(styles.max, styles.value)}>
-          max: <code>{max}</code>
+          {labels.max}: <code>{max}</code>
         </div>
       )}
       {defaultVal !== undefined && (
         <div className={clsx(styles.default, styles.value)}>
-          default: <code>{defaultVal}</code>
+          {labels.default}: <code>{defaultVal}</code>
         </div>
       )}
     </div>
