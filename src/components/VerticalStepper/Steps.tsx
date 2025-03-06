3475fc2178970b8764c949d563e1eb881e917181
VitaliiSalinskyi <v.salinskiy@3commas.io>
2025-03-06 13:54:10 +0100

    chore: restrict rca stepper width to 800px
    - add restrictedWidth prop to Steps

--- src/components/VerticalStepper/Steps.tsx
+++ src/components/VerticalStepper/Steps.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./Steps.module.css";
 
-type Props = { children: React.ReactNode };
+type Props = { children: React.ReactNode; restrictedWidth?: number };
 
-export const Steps = ({ children }: Props) => (
-  <div className={styles.steps}>{children}</div>
+export const Steps = ({ children, restrictedWidth }: Props) => (
+  <div
+    className={styles.steps}
+    style={{ maxWidth: restrictedWidth ? `${restrictedWidth}px` : "none" }}
+  >
+    {children}
+  </div>
 );
