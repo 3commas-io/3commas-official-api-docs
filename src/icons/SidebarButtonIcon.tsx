bdbf92f80f718b0e908930d588beb0338484d091
Vitalii Salinskiy <v.salinskiy@3commas.io>
2025-04-08 21:18:53 +0200

    BOST-214 fix: resolve sidebar flickering & packages volunerabilities

--- src/icons/SidebarButtonIcon.tsx
+++ src/icons/SidebarButtonIcon.tsx
@@ -1,12 +1,11 @@
 export const SidebarButtonIcon = () => (
-    <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none">
-
-      <path
-          d="M5 3H19M5 3C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21M5 3H9M19 3C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21M19 3H9M19 21H5M19 21H9M5 21H9M9 3V21"
-          stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-      ></path>
-    </svg>
+  <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+    <path
+      d="M5 3H19M5 3C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21M5 3H9M19 3C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21M19 3H9M19 21H5M19 21H9M5 21H9M9 3V21"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
 );
