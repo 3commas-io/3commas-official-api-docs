3f106cb1b8dd10c80c9b529964aca8b7d3799112
Vitalii Salinskiy <v.salinskiy@3commas.io>
2025-04-10 20:54:40 +0200

    BOST-214 fix: resolve sidebar flickering

--- src/theme/DocRoot/Layout/Sidebar/index.tsx
+++ src/theme/DocRoot/Layout/Sidebar/index.tsx
@@ -1,20 +1,19 @@
 import React from "react";
-import BrowserOnly from "@docusaurus/BrowserOnly";
 import clsx from "clsx";
-import type SidebarType from "@theme/DocRoot/Layout/Sidebar";
 import Sidebar from "@theme-original/DocRoot/Layout/Sidebar";
-import type { WrapperProps } from "@docusaurus/types";
 
 import { useSidebarContext } from "@site/src/context/SidebarContext";
 import SidebarHoverZone from "@site/src/components/SidebarHoverZone/SidebarHoverZone";
-
 import styles from "./styles.module.css";
+
+import type SidebarType from "@theme/DocRoot/Layout/Sidebar";
+import type { WrapperProps } from "@docusaurus/types";
 
 type Props = WrapperProps<typeof SidebarType>;
 
 export default function SidebarWrapper(props: Props): JSX.Element {
   const { isOpen, isPinned, scheduleClose, cancelScheduledClose } =
-      useSidebarContext();
+    useSidebarContext();
 
   const handleMouseEnter = () => {
     cancelScheduledClose();
@@ -27,37 +26,33 @@ export default function SidebarWrapper(props: Props): JSX.Element {
   };
 
   return (
-      <BrowserOnly fallback={<Sidebar {...props} />}>
-        {() => (
-            <>
-              {window.innerWidth > 996 ? (
-                  <>
-                    {!isOpen && !isPinned && <SidebarHoverZone />}
-                    <div
-                        className={clsx(
-                            styles.sidebarBase,
-                            isPinned ? styles.sidebarPinned : styles.sidebarUnpinned,
-                        )}
-                        onMouseEnter={handleMouseEnter}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                      <div
-                          className={clsx(
-                              styles.sidebarAnimated,
-                              isOpen
-                                  ? styles.sidebarAnimatedOpen
-                                  : styles.sidebarAnimatedClosed,
-                          )}
-                      >
-                        {isOpen && <Sidebar {...props} />}
-                      </div>
-                    </div>
-                  </>
-              ) : (
-                  <Sidebar {...props} />
-              )}
-            </>
-        )}
-      </BrowserOnly>
+    <>
+      <div className={styles.sidebarDesktop}>
+        {!isOpen && !isPinned && <SidebarHoverZone />}
+        <div
+          className={clsx(
+            styles.sidebarBase,
+            isPinned ? styles.sidebarPinned : styles.sidebarUnpinned,
+          )}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div
+            className={clsx(
+              styles.sidebarAnimated,
+              isOpen
+                ? styles.sidebarAnimatedOpen
+                : styles.sidebarAnimatedClosed,
+            )}
+          >
+            {isOpen && <Sidebar {...props} />}
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.sidebarMobile}>
+        <Sidebar {...props} />
+      </div>
+    </>
   );
 }
