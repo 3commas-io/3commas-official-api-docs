ed4c6b0dcf4610e7bcaf0b76ebaea178dcc29be6
Vitalii-Salinskyi <v.salinskiy@3commas.io>
2025-04-16 18:46:38 +0200

    BOST-242 refactor: update home page

--- src/components/HomePageItem/HomePageItem.tsx
+++ src/components/HomePageItem/HomePageItem.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
 import Link from "@docusaurus/Link";
-import styles from "./HomePageItem.module.css";
-import HomePageIcons from "@site/src/icons/HomePageIcons";
 import clsx from "clsx";
 
-interface HomePageItemData {
+import { HomePageIcon } from "@site/src/icons/HomePageIcon";
+import styles from "./HomePageItem.module.css";
+
+import type { IconKeys } from "@site/src/utils/icons";
+
+export interface HomePageItemData {
   title: string;
   description: string;
-  icon: string;
+  icon: IconKeys;
   link: string;
 }
 
@@ -15,35 +17,14 @@ type Props = {
   data: HomePageItemData;
 };
 
-const HomePageItem = ({ data }: Props) => {
-  useEffect(() => {
-    const paginatorNav = document.querySelector(
-      ".pagination_zBJ0",
-    ) as HTMLElement;
-
-    if (paginatorNav) {
-      paginatorNav.style.setProperty("display", "none", "important");
-    }
-    const breadcrumbs = document.querySelector(
-      ".breadcrumbsContainer_Ar0W",
-    ) as HTMLElement;
-
-    if (breadcrumbs) {
-      breadcrumbs.style.display = "none";
-    }
-  }, []);
-
-  return (
-    <Link to={data.link} className={clsx(styles.homePageItemLink, "home-line")}>
-      <div className={styles.homePageItemIconWrapper}>
-        <HomePageIcons icon={data.icon} />
-      </div>
-      <div className={styles.homePageItemContentWrapper}>
-        <p className={styles.homePageItemTitle}>{data.title}</p>
-        <p className={styles.homePageItemDescription}>{data.description}</p>
-      </div>
-    </Link>
-  );
-};
-
-export default HomePageItem;
+export const HomePageItem = ({ data }: Props) => (
+  <Link to={data.link} className={clsx(styles.homePageItemLink, "home-line")}>
+    <div className={styles.homePageItemIconWrapper}>
+      <HomePageIcon icon={data.icon} />
+    </div>
+    <div className={styles.homePageItemContentWrapper}>
+      <p className={styles.homePageItemTitle}>{data.title}</p>
+      <p className={styles.homePageItemDescription}>{data.description}</p>
+    </div>
+  </Link>
+);
